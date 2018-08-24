cbf9ecfae4feca2d337cd14970ab1b961fccc54b
zdm <dr-ming@qq.com>
2018-08-24 15:29:50 +0800

    修改支持有赞云V3版接口

--- app/Http/Controllers/Api/YzyController.php
+++ app/Http/Controllers/Api/YzyController.php
@@ -60,162 +60,159 @@ class YzyController extends Controller
         // msg内容经过 urlencode 编码，进行解码
         $msg = json_decode(urldecode($msg), true);
 
-        if ($data['type'] == 'TRADE_ORDER_STATE') {
-            // 读取订单信息
-            $yzy = new Yzy();
-            $result = $yzy->getTradeByTid($msg['tid']);
-            if (isset($result['error_response'])) {
-                Log::info('【有赞云】回调订单信息错误：' . $result['error_response']['msg']);
-                exit();
-            }
+        // 有赞V3接口
 
-            $payment = Payment::query()->where('qr_id', $result['response']['trade']['qr_id'])->first();
+        // 读取订单信息
+        $yzy = new Yzy();
+
+        // 等待支付
+        if ($data['status'] == 'WAIT_BUYER_PAY') {
+            Log::info('【有赞云】等待支付');
+            exit();
+        }
+
+        // 成功付款
+        if ($data['status'] == 'PAID') {
+            $qr_id = $msg['qr_info']['qr_id'];
+            Log::info('【有赞云】付款成功,qr_id=' . $msg['qr_info']['qr_id']);
+            $payment = Payment::query()->where('qr_id', $qr_id)->first();
             if (!$payment) {
-                Log::info('【有赞云】回调订单不存在');
+                Log::error('【有赞云】回调订单不存在,qr_id=' . $qr_id);
+                exit();
+            }
+            //FIXME: 这一步检查貌似没必要exit
+            if ($payment->status != '0') {
+                Log::warning('【有赞云】回调订单状原状态不是[等待支付]');
                 exit();
             }
 
-            // 等待支付
-            if ($data['status'] == 'WAIT_BUYER_PAY') {
-                Log::info('【有赞云】等待支付' . urldecode($data['msg']));
+            // 处理订单
+            DB::beginTransaction();
+            try {
+                // 更新支付单
+                $payment->pay_way =  $msg['full_order_info']['source_info']['source']['platform'] == 'wx' ? 1 : 2; // 1-微信、2-支付宝
+                $payment->status = 1;
+                $payment->save();
+
+                // 更新订单
+                $order = Order::query()->with(['user'])->where('oid', $payment->oid)->first();
+                $order->status = 2;
+                $order->save();
+
+                // 如果买的是套餐，则先将之前购买的所有套餐置都无效，并扣掉之前所有套餐的流量
+                $goods = Goods::query()->where('id', $order->goods_id)->first();
+                if ($goods->type == 2) {
+                    $existOrderList = Order::query()
+                        ->with(['goods'])
+                        ->whereHas('goods', function ($q) {
+                            $q->where('type', 2);
+                        })
+                        ->where('user_id', $order->user_id)
+                        ->where('oid', '<>', $order->oid)
+                        ->where('is_expire', 0)
+                        ->where('status', 2)
+                        ->get();
+
+                    foreach ($existOrderList as $vo) {
+                        Order::query()->where('oid', $vo->oid)->update(['is_expire' => 1]);
+                        User::query()->where('id', $order->user_id)->decrement('transfer_enable', $vo->goods->traffic * 1048576);
+                    }
+                }
+
+                // 把商品的流量加到账号上
+                User::query()->where('id', $order->user_id)->increment('transfer_enable', $goods->traffic * 1048576);
+
+                // 套餐就改流量重置日，流量包不改
+                if ($goods->type == 2) {
+                    // 将商品的有效期和流量自动重置日期加到账号上
+                    $traffic_reset_day = in_array(date('d'), [29, 30, 31]) ? 28 : abs(date('d'));
+                    User::query()->where('id', $order->user_id)->update(['traffic_reset_day' => $traffic_reset_day, 'expire_time' => date('Y-m-d', strtotime("+" . $goods->days . " days", strtotime($order->user->expire_time))), 'enable' => 1]);
+                } else {
+                    // 将商品的有效期和流量自动重置日期加到账号上
+                    User::query()->where('id', $order->user_id)->update(['expire_time' => date('Y-m-d', strtotime("+" . $goods->days . " days")), 'enable' => 1]);
+                }
+
+                // 写入用户标签
+                if ($goods->label) {
+                    // 用户默认标签
+                    $defaultLabels = [];
+                    if ($this->systemConfig['initial_labels_for_user']) {
+                        $defaultLabels = explode(',', $this->systemConfig['initial_labels_for_user']);
+                    }
+
+                    // 取出现有的标签
+                    $userLabels = UserLabel::query()->where('user_id', $order->user_id)->pluck('label_id')->toArray();
+                    $goodsLabels = GoodsLabel::query()->where('goods_id', $order->goods_id)->pluck('label_id')->toArray();
+
+                    // 标签去重
+                    $newUserLabels = array_merge($userLabels, $goodsLabels, $defaultLabels);
+                    $newUserLabels = array_unique($newUserLabels);
+                    $newUserLabels = array_values($newUserLabels);
+
+                    // 删除用户所有标签
+                    UserLabel::query()->where('user_id', $order->user_id)->delete();
+
+                    // 生成标签
+                    foreach ($newUserLabels as $vo) {
+                        $obj = new UserLabel();
+                        $obj->user_id = $order->user_id;
+                        $obj->label_id = $vo;
+                        $obj->save();
+                    }
+                }
+
+                // 写入返利日志
+                if ($order->user->referral_uid) {
+                    $referralLog = new ReferralLog();
+                    $referralLog->user_id = $order->user_id;
+                    $referralLog->ref_user_id = $order->user->referral_uid;
+                    $referralLog->order_id = $order->oid;
+                    $referralLog->amount = $order->amount;
+                    $referralLog->ref_amount = $order->amount * $this->systemConfig['referral_percent'];
+                    $referralLog->status = 0;
+                    $referralLog->save();
+                }
+
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+
+                Log::info('【有赞云】更新支付单和订单异常：' . $e->getMessage());
+            }
+
+            exit();
+        }
+
+        // 超时自动关闭订单
+        if ($data['status'] == 'TRADE_CLOSED') {
+            if ($payment->status != 0) {
+                Log::info('【有赞云】自动关闭支付单异常，本地支付单状态不正确');
                 exit();
             }
 
-            // 交易成功
-            if ($data['status'] == 'TRADE_SUCCESS') {
-                if ($payment->status != '0') {
-                    Log::info('【有赞云】回调订单状态不正确');
-                    exit();
-                }
-
-                // 处理订单
-                DB::beginTransaction();
-                try {
-                    // 更新支付单
-                    $payment->pay_way = $msg['pay_type'] == '微信支付' ? 1 : 2; // 1-微信、2-支付宝
-                    $payment->status = 1;
-                    $payment->save();
-
-                    // 更新订单
-                    $order = Order::query()->with(['user'])->where('oid', $payment->oid)->first();
-                    $order->status = 2;
-                    $order->save();
-
-                    // 如果买的是套餐，则先将之前购买的所有套餐置都无效，并扣掉之前所有套餐的流量
-                    $goods = Goods::query()->where('id', $order->goods_id)->first();
-                    if ($goods->type == 2) {
-                        $existOrderList = Order::query()
-                            ->with(['goods'])
-                            ->whereHas('goods', function ($q) {
-                                $q->where('type', 2);
-                            })
-                            ->where('user_id', $order->user_id)
-                            ->where('oid', '<>', $order->oid)
-                            ->where('is_expire', 0)
-                            ->where('status', 2)
-                            ->get();
-
-                        foreach ($existOrderList as $vo) {
-                            Order::query()->where('oid', $vo->oid)->update(['is_expire' => 1]);
-                            User::query()->where('id', $order->user_id)->decrement('transfer_enable', $vo->goods->traffic * 1048576);
-                        }
-                    }
-
-                    // 把商品的流量加到账号上
-                    User::query()->where('id', $order->user_id)->increment('transfer_enable', $goods->traffic * 1048576);
-
-                    // 套餐就改流量重置日，流量包不改
-                    if ($goods->type == 2) {
-                        // 将商品的有效期和流量自动重置日期加到账号上
-                        $traffic_reset_day = in_array(date('d'), [29, 30, 31]) ? 28 : abs(date('d'));
-                        User::query()->where('id', $order->user_id)->update(['traffic_reset_day' => $traffic_reset_day, 'expire_time' => date('Y-m-d', strtotime("+" . $goods->days . " days", strtotime($order->user->expire_time))), 'enable' => 1]);
-                    } else {
-                        // 将商品的有效期和流量自动重置日期加到账号上
-                        User::query()->where('id', $order->user_id)->update(['expire_time' => date('Y-m-d', strtotime("+" . $goods->days . " days")), 'enable' => 1]);
-                    }
-
-                    // 写入用户标签
-                    if ($goods->label) {
-                        // 用户默认标签
-                        $defaultLabels = [];
-                        if ($this->systemConfig['initial_labels_for_user']) {
-                            $defaultLabels = explode(',', $this->systemConfig['initial_labels_for_user']);
-                        }
-
-                        // 取出现有的标签
-                        $userLabels = UserLabel::query()->where('user_id', $order->user_id)->pluck('label_id')->toArray();
-                        $goodsLabels = GoodsLabel::query()->where('goods_id', $order->goods_id)->pluck('label_id')->toArray();
-
-                        // 标签去重
-                        $newUserLabels = array_merge($userLabels, $goodsLabels, $defaultLabels);
-                        $newUserLabels = array_unique($newUserLabels);
-                        $newUserLabels = array_values($newUserLabels);
-
-                        // 删除用户所有标签
-                        UserLabel::query()->where('user_id', $order->user_id)->delete();
-
-                        // 生成标签
-                        foreach ($newUserLabels as $vo) {
-                            $obj = new UserLabel();
-                            $obj->user_id = $order->user_id;
-                            $obj->label_id = $vo;
-                            $obj->save();
-                        }
-                    }
-
-                    // 写入返利日志
-                    if ($order->user->referral_uid) {
-                        $referralLog = new ReferralLog();
-                        $referralLog->user_id = $order->user_id;
-                        $referralLog->ref_user_id = $order->user->referral_uid;
-                        $referralLog->order_id = $order->oid;
-                        $referralLog->amount = $order->amount;
-                        $referralLog->ref_amount = $order->amount * $this->systemConfig['referral_percent'];
-                        $referralLog->status = 0;
-                        $referralLog->save();
-                    }
-
-                    DB::commit();
-                } catch (\Exception $e) {
-                    DB::rollBack();
-
-                    Log::info('【有赞云】更新支付单和订单异常：' . $e->getMessage());
-                }
-
+            $order = Order::query()->where('oid', $payment->oid)->first();
+            if ($order->status != 0) {
+                Log::info('【有赞云】自动关闭支付单异常，本地订单状态不正确');
                 exit();
             }
 
-            // 超时自动关闭订单
-            if ($data['status'] == 'TRADE_CLOSED') {
-                if ($payment->status != 0) {
-                    Log::info('【有赞云】自动关闭支付单异常，本地支付单状态不正确');
-                    exit();
-                }
+            DB::beginTransaction();
+            try {
+                // 关闭支付单
+                $payment->status = -1;
+                $payment->save();
 
-                $order = Order::query()->where('oid', $payment->oid)->first();
-                if ($order->status != 0) {
-                    Log::info('【有赞云】自动关闭支付单异常，本地订单状态不正确');
-                    exit();
-                }
+                // 关闭订单
+                $order->status = -1;
+                $order->save();
 
-                DB::beginTransaction();
-                try {
-                    // 关闭支付单
-                    $payment->status = -1;
-                    $payment->save();
-
-                    // 关闭订单
-                    $order->status = -1;
-                    $order->save();
-
-                    DB::commit();
-                } catch (\Exception $e) {
-                    DB::rollBack();
-                    Log::info('【有赞云】更新支付单和订单异常');
-                }
-
-                exit();
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollBack();
+                Log::info('【有赞云】更新支付单和订单异常');
             }
+
+            exit();
         }
 
         if ($data['type'] == 'TRADE') {
